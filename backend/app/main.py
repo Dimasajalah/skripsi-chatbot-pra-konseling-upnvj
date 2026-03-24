29110bf08945847c40bd9abfd7d99d898bbe81b4
Dimasajalah <89116262+Dimasajalah@users.noreply.github.com>
2026-03-24 10:09:32 +0700

    Change CORS policy to allow all origins
    Updated CORS middleware to allow all origins.

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,13 +16,7 @@ def init_schedules():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://192.168.1.100:5173",  
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
